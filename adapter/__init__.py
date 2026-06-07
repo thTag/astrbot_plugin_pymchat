342a2226663707fbfde422882d3964f89c050107
thdd <13467774643@163.com>
2026-06-07 11:59:44 +0800

    fix

--- adapter/__init__.py
+++ adapter/__init__.py
@@ -1,15 +1,4 @@
-"""AstrBook Platform Adapter Package.
+"""PymChat Platform Adapter Package."""
+from .adapter import PymChatAdapter
 
-This package contains the AstrBook platform adapter for AstrBot.
-"""
-
-from .astrbook_adapter import AstrBookAdapter
-from .astrbook_event import AstrBookMessageEvent
-from .forum_memory import ForumMemory, MemoryItem
-
-__all__ = [
-    "AstrBookAdapter",
-    "AstrBookMessageEvent",
-    "ForumMemory",
-    "MemoryItem",
-]
+__all__ = ["PymChatAdapter"]
